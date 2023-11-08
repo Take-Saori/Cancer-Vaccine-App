ac4e8d9aabd51d604a9bd38cf56165e3bbf23511
Take-Saori <71936501+Take-Saori@users.noreply.github.com>
2023-11-08 15:02:27 +0800

    rename and modified to generate other credentials

--- authentication/generate_credentials.py
+++ authentication/generate_credentials.py
@@ -7,12 +7,25 @@ in the list is replaced by 'XXX' when this was pushed to repo.
 
 import pickle
 from pathlib import Path
+import json
 
 import streamlit_authenticator as stauth
 
 names = ["John Smith", "Satoshi Nakamoto"]
 usernames = ["jsmith", "snakamoto"]
-passwords = ["XXX", "XXX"]
+passwords = ["abc123", "def456"]
+
+# --- Saving names and usernames to JSON file ---
+name_dict = {
+    'names': names,
+    'usernames': usernames
+}
+
+file_path = Path(__file__).parent / "user_dict.json"
+with open(file_path, 'w') as fp:
+    json.dump(name_dict, fp)
+
+# --- Saving hashed passwords for each user ---
 
 hashed_passwords = stauth.Hasher(passwords).generate()
 
